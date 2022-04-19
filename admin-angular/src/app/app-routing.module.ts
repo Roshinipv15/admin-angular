f9f9d331719858f2b33d352ed4a51b561fb696a4
Roshinipv15 <91727558+Roshinipv15@users.noreply.github.com>
2022-04-19 17:06:42 +0530

    create component

--- admin-angular/src/app/app-routing.module.ts
+++ admin-angular/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
+import { createComponent } from '@angular/compiler/src/core';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './create/create.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +14,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'create', component: CreateComponent },
 ];
 
 @NgModule({
